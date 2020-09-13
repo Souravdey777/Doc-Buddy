04a20dcf43c1ab024e4ee662ce5bbac47b6c808a
Sourav Dey <piyush.kolkata@gmail.com>
2020-09-13 23:39:00 +0530

    Updates

---
+++ precache-manifest.f348b04c0502e71c5ffc80b73f0d59bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08a827970c3710e9777cf402d0c4517",
+    "url": "/Doc-Buddy/index.html"
+  },
+  {
+    "revision": "943e1b31811e90f41d10",
+    "url": "/Doc-Buddy/static/css/main.2d8d376b.chunk.css"
+  },
+  {
+    "revision": "0b8c02e1b5eeb6001f14",
+    "url": "/Doc-Buddy/static/js/2.1fcfdf76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Doc-Buddy/static/js/2.1fcfdf76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943e1b31811e90f41d10",
+    "url": "/Doc-Buddy/static/js/main.2da62757.chunk.js"
+  },
+  {
+    "revision": "aa5812d2887d0e271cfe",
+    "url": "/Doc-Buddy/static/js/runtime-main.96be0659.js"
+  }
+]);
